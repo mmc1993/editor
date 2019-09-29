d90c48af3fee6c4a84953f81f13050a0f1407eb6
lexinyu <lexinyu@xingfeiinc.com>
2019-09-29 09:57:45 +0800

    智能指针

--- app/editor/include.h
+++ app/editor/include.h
@@ -2,3 +2,18 @@
 
 #include "base.h"
 #include "global.h"
+
+template <class T>
+using UniquePtr = std::unique_ptr<T>;
+
+template <class T>
+using SharePtr = std::shared_ptr<T>;
+
+template <class T>
+using WeakPtr = std::weak_ptr<T>;
+
+template <class T, class U>
+SharePtr<T> CastPtr(U & ptr)
+{
+    return std::reinterpret_pointer_cast<T>(ptr);
+}
