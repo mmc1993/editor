89b19def9f57083b51da55812c939c82685ab1b7
lexinyu <lexinyu@xingfeiinc.com>
2019-10-14 11:50:15 +0800

    引入ID关联GLOBject

--- app/editor/system/editor_sys/project.cpp
+++ app/editor/system/editor_sys/project.cpp
@@ -1,28 +1,63 @@
 #include "project.h"
 #include "component/gl_object.h"
 
+Project::Project(): _acc(0)
+{ }
+
 void Project::New(const std::string & url)
 {
-    _root.reset(new GLObject());
-    _url  = url;
+    _acc = 0x0;
+    _url = url;
+    _root.reset(new GLObject(_acc));
 }
 
-bool Project::Load(const std::string & url)
+void Project::Load(const std::string & url)
 {
     ASSERT_LOG(_root == nullptr, url.c_str());
     std::ifstream is(url);
     ASSERT_LOG(is, url.c_str());
+    tools::Deserialize(is,_acc);
     _root.reset(new GLObject());
     _root->DecodeBinary(is);
     _url = url;
     is.close();
-    return true;
+
+    //  Éú³ÉObject Map
+    std::deque<SharePtr<GLObject>> list{ _root };
+    while (!list.empty())
+    {
+        auto & front = list.front();
+        for (auto & object : front->GetObjects())
+        {
+            list.push_back(object);
+        }
+        _objects.insert(std::make_pair(front->GetID(), front));
+        list.pop_front();
+    }
 }
 
 void Project::Save(const std::string & url)
 {
     ASSERT_LOG(_root != nullptr, url.c_str());
     std::ofstream os(url.empty()? _url : url);
+    tools::Serialize(os, _acc);
     _root->EncodeBinary(os);
     os.close();
+}
+
+SharePtr<GLObject> Project::NewObject()
+{
+    auto object = std::create_ptr<GLObject>(++_acc);
+    _objects.insert(std::make_pair(_acc, object));
+    return object;
+}
+
+void Project::DelObject(const uint & id)
+{
+    _objects.erase(id);
+}
+
+void Project::DelObject(const SharePtr<GLObject> & object)
+{
+    DelObject(object->GetID());
 }
--- app/editor/system/editor_sys/project.h
+++ app/editor/system/editor_sys/project.h
@@ -2,19 +2,33 @@
 
 #include "../../include.h"
 
+#ifdef GetObject
+#undef GetObject
+#endif
+
 class GLObject;
 
 class Project {
 public:
+    Project();
     bool IsOpen() const { return _root != nullptr; }
     const std::string & GetURL() const { return _url; }
     const std::string & GetDIR() const { return tools::GetFileDir(_url); }
+    
     void New(const std::string & url);
-    bool Load(const std::string & url);
+    void Load(const std::string & url);
     void Save(const std::string & url);
-    const SharePtr<GLObject> & GetRoot() { return _root; }
+
+    const SharePtr<GLObject> & GetRoot()          { return _root; }
+    const SharePtr<GLObject> & GetObject(uint id) { return _objects.at(id); }
+
+    SharePtr<GLObject> NewObject();
+    void DelObject(const uint &id);
+    void DelObject(const SharePtr<GLObject> & object);
 
 private:
+    std::map<uint, SharePtr<GLObject>> _objects;
     SharePtr<GLObject> _root;
     std::string         _url;
+    uint                _acc;
 };
