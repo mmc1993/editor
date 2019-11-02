18b1d9b31ede8d1d7971c44212b00fbab34c13f7
mmc1206 <1257335379@qq.com>
2019-11-02 18:01:44 +0800

    改进序列化

--- app/editor/system/ui_sys/ui_object/ui_object_gl_canvas.cpp
+++ app/editor/system/ui_sys/ui_object/ui_object_gl_canvas.cpp
@@ -45,6 +45,7 @@ void UIObjectGLCanvas::HandlePostCommands()
             command.Call(nullptr);
             Post(command.mProgram, command.mTransform);
             command.mMesh->Draw(GL_TRIANGLES);
+            // command.mMesh->Draw(GL_LINES);
         }
     }
 }
