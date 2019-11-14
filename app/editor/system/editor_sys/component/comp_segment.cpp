019c9a1c0abfa797acafe3a8155dd8a9eee8614a
lexinyu <lexinyu@xingfeiinc.com>
2019-11-14 18:30:35 +0800

    贝塞尔曲线

--- app/editor/system/editor_sys/component/comp_segment.cpp
+++ app/editor/system/editor_sys/component/comp_segment.cpp
@@ -69,7 +69,7 @@ bool CompSegment::OnModifyProperty(const std::any & oldValue, const std::any & n
     }
     else if (title == "Smooth")
     {
-        if (auto value = std::any_cast<float>(newValue); value != 0)
+        if (auto value = std::any_cast<float>(newValue); value > 0)
         {
             _smooth = value;
             _update |= kSegment;
@@ -106,49 +106,102 @@ void CompSegment::Update()
 
 void CompSegment::GenSegm()
 {
-    if (_smooth != 1.0f)
+    //  贝塞尔插值
+    std::vector<glm::vec2> ctrlPoints{ _trackPoints.front() };
+    if (_trackPoints.size() > 2)
     {
-        _segments.clear();
-        auto points = _trackPoints;
-        std::sort(points.begin(), points.end(), 
-            [](const auto & a, const auto & b) { return a.x < b.x; });
-        auto iter = std::unique(points.begin(), points.end(), 
-            [](const auto & a, const auto & b) { return a.x == b.x; });
-        points.erase(iter,  points.end());
-        if (points.size() < 2) { return; }
-
-        auto beg = points.front().x;
-        auto end = points.back().x;
-        auto distance  = end - beg;
-        auto step  = distance * _smooth;
-        auto count = int(1.0f / _smooth);
-        for (auto i = 0; i != count; ++i)
+        for (auto i = 0; i != _trackPoints.size() - 2; ++i)
         {
-            auto y = 0.0f;
-            auto x = beg + i * step;
-            for (auto i0 = 0; i0 != points.size(); ++i0)
-            {
-                auto selfY = points.at(i0).y;
-                auto m0 = 1.0f; //  分子
-                auto m1 = 1.0f; //  分母
-                for (auto i1 = 0; i1 != points.size(); ++i1)
-                {
-                    if (i0 != i1)
-                    {
-                        m0 *= (x -               points.at(i1).x);
-                        m1 *= (points.at(i0).x - points.at(i1).x);
-                    }
-                }
-                y += m0 / m1 * selfY;
-            }
-            _segments.emplace_back(x, y);
+            auto & a = _trackPoints.at(i    );
+            auto & b = _trackPoints.at(i + 1);
+            auto & c = _trackPoints.at(i + 2);
+
+            auto mid0 = glm::lerp(a, b, 0.5f);
+            auto mid1 = glm::lerp(b, c, 0.5f);
+
+            auto abLen = glm::length(b - a);
+            auto bcLen = glm::length(c - b);
+            auto sunLen = abLen + bcLen;
+
+            auto offset = b - glm::lerp(mid0, mid1, abLen / sunLen);
+            ctrlPoints.emplace_back(mid0 + offset);
+            ctrlPoints.emplace_back(mid1 + offset);
         }
-        _segments.emplace_back(points.back());
+    }
+    ctrlPoints.emplace_back(_trackPoints.back());
+
+    _segments.clear();
+    if (ctrlPoints.size() == 2)
+    {
+        //  1次贝塞尔曲线
+        _segments.emplace_back(ctrlPoints.at(0));
+        _segments.emplace_back(ctrlPoints.at(1));
     }
     else
     {
-        _segments = _trackPoints;
+        //  N次贝塞尔曲线
+        auto count = iint(1.0f / _smooth);
+        for (auto s = 0; s != count; ++s)
+        {
+            auto t = s * _smooth;
+            auto beg = glm::lerp(ctrlPoints.at(0), ctrlPoints.at(1), t);
+            auto end = glm::lerp(ctrlPoints.at(2), ctrlPoints.at(3), t);
+            for (auto i = 3; i != ctrlPoints.size() - 2; ++i)
+            {
+                auto & a = ctrlPoints.at(i    );
+                auto & b = ctrlPoints.at(i + 1);
+                beg = glm::lerp(beg, end, t);
+                end = glm::lerp(a, b, t);
+            }
+            _segments.emplace_back(glm::lerp(beg, end, t));
+        }
     }
+
+
+    //  拉卡朗日插值
+    //if (_smooth != 1.0f)
+    //{
+    //    _segments.clear();
+    //    auto points = _trackPoints;
+    //    std::sort(points.begin(), points.end(), 
+    //        [](const auto & a, const auto & b) { return a.x < b.x; });
+    //    auto iter = std::unique(points.begin(), points.end(), 
+    //        [](const auto & a, const auto & b) { return a.x == b.x; });
+    //    points.erase(iter,  points.end());
+    //    if (points.size() < 2) { return; }
+
+    //    auto beg = points.front().x;
+    //    auto end = points.back().x;
+    //    auto distance  = end - beg;
+    //    auto step  = distance * _smooth;
+    //    auto count = int(1.0f / _smooth);
+    //    for (auto i = 0; i != count; ++i)
+    //    {
+    //        auto y = 0.0f;
+    //        auto x = beg + i * step;
+    //        for (auto i0 = 0; i0 != points.size(); ++i0)
+    //        {
+    //            auto selfY = points.at(i0).y;
+    //            auto m0 = 1.0f; //  分子
+    //            auto m1 = 1.0f; //  分母
+    //            for (auto i1 = 0; i1 != points.size(); ++i1)
+    //            {
+    //                if (i0 != i1)
+    //                {
+    //                    m0 *= (x -               points.at(i1).x);
+    //                    m1 *= (points.at(i0).x - points.at(i1).x);
+    //                }
+    //            }
+    //            y += m0 / m1 * selfY;
+    //        }
+    //        _segments.emplace_back(x, y);
+    //    }
+    //    _segments.emplace_back(points.back());
+    //}
+    //else
+    //{
+    //    _segments = _trackPoints;
+    //}
 }
 
 void CompSegment::GenMesh()
@@ -205,7 +258,7 @@ void CompSegment::OnInsertTrackPoint(const size_t index, const glm::vec2 & point
 
 void CompSegment::OnDeleteTrackPoint(const size_t index, const glm::vec2 & point)
 {
-    if (_trackPoints.size() > 1)
+    if (_trackPoints.size() > 2)
     {
         _update |= kSegment;
         AddState(StateEnum::kUpdate, true);
