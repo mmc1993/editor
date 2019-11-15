c0bca8008599911f072c9296131094bc3f3c8c9d
lexinyu <lexinyu@xingfeiinc.com>
2019-11-15 17:06:50 +0800

    多边形组件

---
+++ app/editor/system/editor_sys/component/comp_polygon.cpp
@@ -0,0 +1,70 @@
+#include "comp_polygon.h"
+#include "comp_transform.h"
+#include "../../raw_sys/raw.h"
+#include "../../raw_sys/raw_sys.h"
+#include "../../ui_sys/ui_object/ui_object.h"
+
+CompPolygon::CompPolygon()
+{
+    _segments.emplace_back(0, 0);
+    _trackPoints.emplace_back(0, 0);
+    AddState(StateEnum::kModifyTrackPoint, true);
+    AddState(StateEnum::kInsertTrackPoint, true);
+    AddState(StateEnum::kDeleteTrackPoint, true);
+}
+
+void CompPolygon::OnUpdate(UIObjectGLCanvas * canvas, float dt)
+{
+}
+
+const std::string & CompPolygon::GetName()
+{
+    static const std::string name = "Polygon";
+    return name;
+}
+
+void CompPolygon::EncodeBinary(std::ofstream & os)
+{
+    Component::EncodeBinary(os);
+    tools::Serialize(os, _trackPoints);
+}
+
+void CompPolygon::DecodeBinary(std::ifstream & is)
+{
+    Component::DecodeBinary(is);
+    tools::Deserialize(is, _trackPoints);
+}
+
+bool CompPolygon::OnModifyProperty(const std::any & oldValue, const std::any & newValue, const std::string & title)
+{
+    return true;
+}
+
+std::vector<Component::Property> CompPolygon::CollectProperty()
+{
+    return Component::CollectProperty();
+}
+
+void CompPolygon::OnModifyTrackPoint(const size_t index, const glm::vec2 & point)
+{
+    AddState(StateEnum::kUpdate, true);
+    _trackPoints.at(index) = point;
+    _segments.at(index) = point;
+}
+
+void CompPolygon::OnInsertTrackPoint(const size_t index, const glm::vec2 & point)
+{
+    AddState(StateEnum::kUpdate, true);
+    _segments.insert(std::next(_segments.begin(), index), point);
+    _trackPoints.insert(std::next(_trackPoints.begin(), index), point);
+}
+
+void CompPolygon::OnDeleteTrackPoint(const size_t index, const glm::vec2 & point)
+{
+    if (_trackPoints.size() > 1)
+    {
+        _segments.erase(std::next(_segments.begin(), index));
+        _trackPoints.erase(std::next(_trackPoints.begin(), index));
+    }
+}
+
---
+++ app/editor/system/editor_sys/component/comp_polygon.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "component.h"
+#include "../../interface/render.h"
+
+class CompPolygon : public Component {
+public:
+    CompPolygon();
+    virtual void OnUpdate(UIObjectGLCanvas * canvas, float dt) override;
+
+    virtual const std::string & GetName() override;
+    virtual void EncodeBinary(std::ofstream & os) override;
+    virtual void DecodeBinary(std::ifstream & is) override;
+    virtual bool OnModifyProperty(const std::any & oldValue, 
+                                  const std::any & newValue, 
+                                  const std::string & title) override;
+    std::vector<glm::vec2> & GetSegments() { return _segments; }
+
+protected:
+    virtual std::vector<Property> CollectProperty() override;
+
+private:
+    virtual void OnModifyTrackPoint(const size_t index, const glm::vec2 & point) override;
+    virtual void OnInsertTrackPoint(const size_t index, const glm::vec2 & point) override;
+    virtual void OnDeleteTrackPoint(const size_t index, const glm::vec2 & point) override;
+
+private:
+    std::vector<glm::vec2> _segments;
+};
