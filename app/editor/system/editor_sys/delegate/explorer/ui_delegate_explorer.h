38adba99f58df0110ed9f83089adf5c6314d1372
lexinyu <lexinyu@xingfeiinc.com>
2019-12-03 16:38:03 +0800

    实现Explorer

--- app/editor/system/editor_sys/delegate/explorer/ui_delegate_explorer.h
+++ app/editor/system/editor_sys/delegate/explorer/ui_delegate_explorer.h
@@ -2,5 +2,41 @@
 
 #include "../ui_delegate_base.h"
 
+//  资源列表格式:
+//      Type|Path [> Word0 Word1]
+
+//  搜索关键字格式:
+//      Type0 Type1|Word0 Word1
+
 class UIDelegateExplorer : public UIDelegateBase {
+private:
+    virtual bool OnCallEventMessage(UIEventEnum e, const UIEvent::Event & param, const SharePtr<UIObject> & object) override;
+
+    //  界面操作
+    void ListRefresh();
+    void ListClick1();
+    void ListClick2();
+    void ListRClick();
+
+    //  资源操作
+    void RefReference();
+    void ResSetType();
+    void ResRename();
+    void ResDelete();
+
+    //  Serach
+    //      Type0 Type1|Word0 Word1
+
+    //  Menu
+    //      Delete
+    //      Rename
+    //      SetType
+    //      Reference
+
+    //  Show
+    //      Type|Path > Word0 Word1
+
+private:
+    std::map<uint, UIObject *> _id2obj;
+    std::map<UIObject *, uint> _obj2id;
 };
