919f7e9c96ea2ced5f743a2f62d20bb99f5b8264
lexinyu <lexinyu@xingfeiinc.com>
2020-01-20 11:54:35 +0800

    优化接口

--- app/editor/system/editor_sys/component/comp_script.h
+++ app/editor/system/editor_sys/component/comp_script.h
@@ -3,12 +3,5 @@
 #include "../../raw_sys/component.h"
 
 class CompScript : public Component {
-public:
 
-
-private:
-
-protected:
-    virtual void OnStart() = 0;
-    virtual void OnLeave() = 0;
 };
